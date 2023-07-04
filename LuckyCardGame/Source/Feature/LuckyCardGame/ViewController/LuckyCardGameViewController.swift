b1477fc5def694b0323dc0449a82523048fe9e95
ysh <happysh_s2@naver.com>
2023-07-05 01:05:43 +0900

    Initial Project setting

---
+++ LuckyCardGame/Source/Feature/LuckyCardGame/ViewController/LuckyCardGameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LuckyCardGameViewController.swift
+//  LuckyCardGame
+//
+//  Created by 양승현 on 2023/07/05.
+//
+
+import UIKit
+
+class LuckyCardGameViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemRed
+  }
+
+
+}
+
