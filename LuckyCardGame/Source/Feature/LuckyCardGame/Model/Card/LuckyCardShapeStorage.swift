776829a03d1374144ddf33e01ca892c7d078ecfb
ysh <happysh_s2@naver.com>
2023-07-06 00:02:14 +0900

    [Feat] Final 키워드 추가

--- LuckyCardGame/Source/Feature/LuckyCardGame/Model/Card/LuckyCardShapeStorage.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/Model/Card/LuckyCardShapeStorage.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// Card는 Shape를 가질 수 있습니다. 특정 Card에서 보여질 수 있는 모든 shape를 저장합니다.
 /// 추후 게임 상황에 따라 게임 실행 중 추가되거나 삭제될 수 있다고 판단해 유연하게 다룰 수 있도록 추상화했습니다.
-class CardShapeStorage<Shape: Hashable, Value: Any> :CardShapeStorageProtocol {
+final class CardShapeStorage<Shape: Hashable, Value: Any> :CardShapeStorageProtocol {
   
   private(set) var shapes: [Shape:Value] = [:]
   var count: Int {
