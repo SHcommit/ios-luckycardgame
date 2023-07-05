6f2fc6af541b959d0e98fbb2f30545b09eaff6e7
ysh <happysh_s2@naver.com>
2023-07-05 23:59:01 +0900

    [Feat] Card를 변환할 수 있는 프로토콜 추가

--- LuckyCardGame/Source/Feature/LuckyCardGame/Protocol/Card/CardShapeStorageProtocol.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/Protocol/Card/CardShapeStorageProtocol.swift
@@ -5,8 +5,7 @@
 //  Created by 양승현 on 2023/07/05.
 //
 
-import Foundation
-
+/// 카드의 모양은 모양 키 Shape와 값Value를 갖을 수 있습니다.
 protocol CardShapeStorageProtocol  {
   associatedtype Shape: Hashable
   associatedtype Value
