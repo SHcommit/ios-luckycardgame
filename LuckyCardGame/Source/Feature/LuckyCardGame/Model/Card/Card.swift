0d85162c573eb9415982cec316f9ee79b4df8ba2
ysh <happysh_s2@naver.com>
2023-07-07 01:13:49 +0900

    [Refactor] 카드 모양, number 제너릭으로 선언 + 앞, 뒷면 구현

--- LuckyCardGame/Source/Feature/LuckyCardGame/Model/Card/Card.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/Model/Card/Card.swift
@@ -9,23 +9,31 @@
 /// 카드 숫자는 Int타입입니다.
 /// 카드 모양은 게임 유형에 따라 개, 고양이, 말 등이 될 수도 있습니다. 또는 마름모, 원, 하트 등이 될 수 있습니다.
 /// Hashable 타입으로 선언해서, CardShapeStorage 클래스를 통해 Shape타입에 해당하는 값을 출력하거나 화면에 보여줄 수 있습니다.
-class Card<Shape: Hashable> {
+///
+
+class Card<Shape: Hashable, Number: Hashable> {
+  enum Appearance {
+    case front
+    case rear
+  }
+  
   // MARK: - Properties
-  var number: Int
-  var shape: Shape
+  private(set) var number: Number
+  private(set) var shape: Shape
+  private var _appearance: Appearance
+  
+  var appearance: Appearance {
+    get {
+      _appearance
+    } set {
+      _appearance = newValue
+    }
+  }
   
   // MARK: - Lifecycle
-  init(number: Int, shape: Shape) {
+  init(number: Number, shape: Shape, appearance: Appearance) {
     self.number = number
     self.shape = shape
-  }
-}
-
-// MARK: - CardConvertible
-extension Card: CardConvertible {
-  // 추후 에러 처리TODO: - CardShapeStorage error 처리
-  func description(with cardShapeStorage: CardShapeStorageImpl<Shape, String>) -> String {
-    let shape = cardShapeStorage.shapes[shape] ?? ""
-    return "\(shape.toUnicodeTypeString())\(String(format: "%02d", number))"
+    self._appearance = appearance
   }
 }
