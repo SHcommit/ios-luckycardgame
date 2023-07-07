2e23f4fdef01f329e51410309d6eb625163f9064
ysh <happysh_s2@naver.com>
2023-07-08 02:38:50 +0900

    [Refactor] 명심! convenience init은 지정 init보다 편하게 호출되야한다.

--- LuckyCardGame/Source/Feature/LuckyCardGame/View/Content/LuckyCardGameContentView.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/View/Content/LuckyCardGameContentView.swift
@@ -7,26 +7,6 @@
 
 import UIKit
 
-/// 플레이어들의 카드를 담당하는 view 입니다.
-///
-/// - Param maximumHeadCount : 가능한 플레이어 인원 수
-/// - Param playerHeadCount : 3 or 4 or 5. 추후 재사용 하기 위해 enum타입으로 정의했습니다.
-///   이에 따라 boardViews가 초기화 됩니다.
-/// - Param playerCardBoardViews : 각각의 플레이어가 가진 카드들의 보드판에 대한 view입니다.
-///   playerHeadCount에 따라 게임 제약 조건이 적용된 화면에 보드 판이 보여집니다.
-/// - Param isInitBoardViews : playerCardBoardViews가 초기화 되지 않았을 경우를 확인하는 computed property
-///
-/// **Notes:**
-/// 1. LuckyCardGameContentView는 layout으로 지정되었습니다. 그렇지만 결국엔 frame이 지정되기 때문에 그 시점의 bounds와 frame을 이용했습니다.
-/// 2. playerHeadCount에 따라 playerCardBoardViews가 초기화 됩니다. 사용자가 3명, 4명, 5명인 경우에 따라 즉시 즉시 초기화 됩니다.
-///
-/// Examples:
-/// ```
-/// let contentView = LuckyCardGameContentView()
-/// contentView.configure(with: .five)
-///
-/// ```
-///
 final class LuckyCardGameContentView: BaseView {
   // MARK: - Constant
   struct Constant {
@@ -45,15 +25,8 @@ final class LuckyCardGameContentView: BaseView {
   }
   
   // MARK: - Lifecycle
-  private init(frame: CGRect) {
+  init(frame: CGRect, playerHeadCount: PlayerHeadCountType) {
     super.init(with: .contentView, frame)
-  }
-  
-  convenience init(
-    frame: CGRect,
-    playerHeadCount: PlayerHeadCountType
-  ) {
-    self.init(frame: frame)
     self.playerHeadCount = playerHeadCount
     setupUI()
   }
@@ -68,7 +41,6 @@ extension LuckyCardGameContentView: LayoutSupport {
     let defaultY: CGFloat = bounds.origin.y
     var y: CGFloat = defaultY
     initBoardViews(y: &y, height: playerGameBoardHeight)
-    
   }
   
   func addSubviews() {
