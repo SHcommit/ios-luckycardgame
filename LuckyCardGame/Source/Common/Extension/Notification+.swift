18bc344acc679a4443b8d58c0d5bce15e6da01f1
ysh <happysh_s2@naver.com>
2023-07-08 00:43:05 +0900

    [Feat] Add a notification name to send a menu changed notification.

---
+++ LuckyCardGame/Source/Common/Extension/Notification+.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+.swift
+//  LuckyCardGame
+//
+//  Created by 양승현 on 2023/07/08.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let playerHeadCountMenuDidChange = Notification
+    .Name("playerHeadCountMenuDidChange")
+}
