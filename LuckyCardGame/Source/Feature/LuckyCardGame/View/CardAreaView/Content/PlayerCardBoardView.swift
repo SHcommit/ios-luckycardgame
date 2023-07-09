81452f445d30f590f7d74ec1be8dd244dd869cb8
ysh <happysh_s2@naver.com>
2023-07-09 21:13:12 +0900

    [Feat] 카드간 interitem spacing, 카드 intrinsic size Constant 구현

--- LuckyCardGame/Source/Feature/LuckyCardGame/View/CardAreaView/Content/PlayerCardBoardView.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/View/CardAreaView/Content/PlayerCardBoardView.swift
@@ -10,7 +10,85 @@ import UIKit
 final class PlayerCardBoardView: BaseRoundView {
   // MARK: - Constant
   struct Constant {
+    static func cardContentWidthWithoutHoriSpacing(
+      from superView: UIView, edgeSpacing: UISpacing
+    ) -> CGFloat {
+      return superView.bounds
+        .width - (edgeSpacing
+          .leading + edgeSpacing.trailing)
+    }
+    
+   
+    
+    // 기준은 사용자 5명일 때 각각의 카드에 대한 interitem spacing = 0을 기준으로 했습니다.
+    // 첫 카드와 보드 edge간 leading spacing = 12 , top spacing = 7
+    static func intrinsicCardSize(from superView: UIView) -> CGSize {
+      let cardContentWidthWithoutHoriSpacing = cardContentWidthWithoutHoriSpacing(
+        from: superView,
+        edgeSpacing: HeadCountIsFive.edgeSpacing)
+      
+      let intrinsicCardHeight = superView.bounds
+        .width - (HeadCountIsFive.edgeSpacing
+          .top + HeadCountIsFive.edgeSpacing.bottom)
+      
+      let intrinsicCardWidth = cardContentWidthWithoutHoriSpacing / CGFloat(
+        PlayerHeadCountType.five.playerCardsCountInBoard)
+      
+      return .init(
+        width: intrinsicCardWidth,
+        height: intrinsicCardHeight)
+    }
+    
+    static func interItemSpacing(
+      from superView: UIView,
+      edgeSpacing: UISpacing,
+      headCount: PlayerHeadCountType
+    ) -> UISpacing {
+      let cardSize = intrinsicCardSize(from: superView)
+      let boardWidthWithoutSpacing = cardContentWidthWithoutHoriSpacing(
+        from: superView,
+        edgeSpacing: edgeSpacing)
+      let playerOwnCardsCount = CGFloat(headCount
+        .playerCardsCountInBoard)
+      let totalWidthCardSize = cardSize.width * playerOwnCardsCount
+      
+      let leftSpacing: CGFloat = (totalWidthCardSize - boardWidthWithoutSpacing) / (playerOwnCardsCount - 1)
+      return .init(leading: leftSpacing)
+    }
+    
+    enum HeadCountIsThree {
+      static let edgeSpacing: UISpacing = .init(
+        leading: 7, top:7, trailing: 7, bottom: 7)
+      static func interItemSpacing(from superView: UIView) -> UISpacing {
+        return Constant.interItemSpacing(
+          from: superView,
+          edgeSpacing: edgeSpacing,
+          headCount: .three)
+      }
+    }
+    
+    enum HeadCountIsFour {
+      static let edgeSpacing: UISpacing = .init(
+        leading: 12, top: 7, trailing: 12, bottom: 7)
+      static func interItemSpacing(from superView: UIView) -> UISpacing {
+        return Constant.interItemSpacing(
+          from: superView,
+          edgeSpacing: edgeSpacing,
+          headCount: .four)
+      }
+      
+    }
+    
+    enum HeadCountIsFive {
+      static let edgeSpacing: UISpacing = .init(
+        leading: 12, top: 7, trailing: 12, bottom: 7)
+      static let spacing: UISpacing = .init()
+    }
   }
+  
+  typealias ConstThree = Constant.HeadCountIsThree
+  typealias ConstFour = Constant.HeadCountIsFour
+  typealias ConstFive = Constant.HeadCountIsFive
   
   // MARK: - Properties
   private var cardViews: [LuckyCardView]!
@@ -74,13 +152,17 @@ private extension PlayerCardBoardView {
 extension PlayerCardBoardView: LayoutSupport {
   func createSubviews() {
     initCards()
+    
   }
   
   func addSubviews() {
+    _=cardViews.map{ addSubview($0) }
   }
 }
 
 // MARK: - LayoutSupport helper
-extension PlayerCardBoardView {
-  
+private extension PlayerCardBoardView {
+  func cardViewFrame(with idx: Int) {
+    
+  }
 }
