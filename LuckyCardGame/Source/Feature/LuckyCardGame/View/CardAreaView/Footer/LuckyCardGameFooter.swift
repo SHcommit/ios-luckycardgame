c680c10b7efc4ce9a496c93fc3006c701f929d3c
ysh <happysh_s2@naver.com>
2023-07-09 19:52:13 +0900

    [Feat] 게임메니져 추가

--- LuckyCardGame/Source/Feature/LuckyCardGame/View/CardAreaView/Footer/LuckyCardGameFooter.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/View/CardAreaView/Footer/LuckyCardGameFooter.swift
@@ -14,15 +14,21 @@ final class LuckyCardGameFooter: BaseRoundView {
     static let bgColor: UIColor = BaseViewPalette.footer.bgColor
   }
   
+  // MARK: - Properties
+  private var gameManager: LuckyCardManager!
+  
   // MARK: - Lifecycle
-  init(frame: CGRect) {
+  init(frame: CGRect, gameManager: LuckyCardManager) {
     super.init(with: .footer, frame)
+    self.gameManager = gameManager
     backgroundColor = Constant.bgColor
     layer.cornerRadius = Constant.radius
   }
   
+  // gameManagerFIXME: - gmaeManager 초기화 x
   required init?(coder: NSCoder) {
     super.init(coder: coder)
+    gameManager = nil
     backgroundColor = Constant.bgColor
     layer.cornerRadius = Constant.radius
   }
