9781e4d7bc0cb758f45ea9bb6e568f1a4e2372f7
shcommit <happysh_s2@naver.com>
2023-07-11 03:21:56 +0900

    [Feat]  CardShapeEnumProtocol's RawRepresentable 준수 + 주석

--- LuckyCardGame/Source/Feature/LuckyCardGame/Model/Card/LuckyCardShapeType.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/Model/Card/LuckyCardShapeType.swift
@@ -5,7 +5,39 @@
 //  Created by 양승현 on 2023/07/05.
 //
 
+/**
+ LuckyCardShapeType는 enum으로 선정했습니다.
+ 카드는 한개의 모양을 갖을 수 있고 여러 case 중 한개의 값을 선정할 수 있는 enum is the best라 생각했습니다.
+ 
+ Notes:
+ 1. unicode는 LuckyCardShape의 Unicode 문자를 반환합니다.
+ 2. 모양의 description은 Emoji string이 반환됩니다.
+ Unicode를 콘솔창에도 출력할 수 있는형태인 UnicodeScalar로 변환해야 합니다.
+ */
 enum LuckyCardShapeType: CardShapeEnumProtocol {
+  init?(rawValue: String) {
+    switch rawValue {
+    case "cat":
+      self = .cat
+    case "cow":
+      self = .cow
+    case "dog":
+      self = .dog
+    default:
+      self = .dog
+    }
+  }
+  
+  var rawValue: String {
+    switch self {
+    case .cat: return "cat"
+    case .cow: return "cow"
+    case .dog: return "dog"
+    }
+  }
+  
+  typealias RawValue = String
+  
   case dog
   case cat
   case cow
