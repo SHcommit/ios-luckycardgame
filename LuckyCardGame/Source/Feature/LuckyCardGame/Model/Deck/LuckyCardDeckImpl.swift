3d61b15a3ef93dc464e5b72a49cc1f0e93f8fa50
shcommit <happysh_s2@naver.com>
2023-07-11 11:57:15 +0900

    [Refactor] 럭키 카드 덱이 할 수 있는 역할은 Deck protocl + 삽입이라고 생각했습니다. 카드를 나눠주는 것은 LuckyCardGameManager가 해야할 일임으로 제거했습ㄴ디ㅏ.

--- LuckyCardGame/Source/Feature/LuckyCardGame/Model/Deck/LuckyCardDeckImpl.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/Model/Deck/LuckyCardDeckImpl.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-/// final 키워드를 붙인 이유는 LuckyCard가 들어있는 Deck이기 때문입니다. 이 클래스는 더 이상 상속을 할 수 없도록 했습니다.
-/// class와 struct 중 참조 타입인 class를 선택한 이유는 struct내부 변수에 참조 타입이 있을 경우 struct가 deinit되어도 참조타입인 멤버변수는
-/// 언제 할당 해제될 지 모르기 때문입니다. 예전에 MVVM 패턴에서 VM은 항상 struct로 구현했고 대부분의 객체들을 struct로 구현했습니다.
-/// 하지만 혹시 struct안에 class타입의 참조 변수 인스턴스를 갖고 있을 경우 위와 같은 사항이 발생된다는 것을 알게 되면서 class 를 선택하게 된 것 같습니다.
-/// 다만 조심 해야 할 부분은 참조 타입이기 때문에 strong reference cycle와 메모리 누수를 방지하기 위해 때로는 weak를 사용해야 한다는 점입니다.
+
 final class LuckyCardDeckImpl: Deck {
   // MARK: - Constant
-  typealias DeckError = LuckyCardDeckError
   typealias Card = LuckyCard
+  typealias DeckError = LuckyCardDeckError
   
   // MARK: - Properties
   var cards: [Card]
+  
+  var description: String {
+    return cards.map { $0.description() }.joined(separator: ", ")
+  }
   
   // MARK: - Lifecycle
   init(cards: [Card]) {
@@ -34,31 +34,5 @@ extension LuckyCardDeckImpl {
       throw DeckError.OutOfRange
     }
     cards.insert(card, at: index)
-  }
-  
-  func description(with manager: LuckyCardManager) -> String {
-    return cards.map {
-      $0.description(with: manager.shapeStorage)
-    }.joined(separator: ", ")
-  }
-}
-
-// MARK: - LuckyCardDeckRule
-extension LuckyCardDeckImpl {
-  func divideCards(
-    in board: PlayerBoardType,
-    with headCount: PlayerHeadCountType
-  ) -> [LuckyCard] {
-    let start = board.toIndex(with: headCount)
-    let end = start + headCount.playerCardsCountInBoard
-    return (start..<end).map { cards[$0] }
-  }
-  
-  func divideBottomCards(
-    with headCount: PlayerHeadCountType
-  ) -> [LuckyCard] {
-    let start = headCount.playerCardsCountInBoard*headCount.toInt
-    let end = start + headCount.bottomCardsCountInBoard
-    return (start..<end).map { cards[$0] }
   }
 }
