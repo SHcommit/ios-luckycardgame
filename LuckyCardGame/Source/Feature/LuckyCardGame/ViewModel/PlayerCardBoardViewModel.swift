b3a127afeacf43885e411f53b61a5dae29494a21
shcommit <happysh_s2@naver.com>
2023-07-11 12:10:31 +0900

    [Style] cardManager -> cardGameManager로 이름 변경

--- LuckyCardGame/Source/Feature/LuckyCardGame/ViewModel/PlayerCardBoardViewModel.swift
+++ LuckyCardGame/Source/Feature/LuckyCardGame/ViewModel/PlayerCardBoardViewModel.swift
@@ -11,24 +11,22 @@ final class PlayerCardBoardViewModel {
   // MARK: - Properties
   private(set) var boardType: PlayerBoardType
   private(set) var player: Player
-  private(set) var gameManager: LuckyCardManager
+  private(set) var gameManager: LuckyCardGameManager
   
   // MARK: - Lifecycle
   init(
     boardType: PlayerBoardType,
-    gameManager: LuckyCardManager
+    gameManager: LuckyCardGameManager
   ) {
     self.boardType = boardType
     self.gameManager = gameManager
     let playerOwnTheDeck: LuckyCardDeckImpl = .init(
-      cards: gameManager.luckyCardDeckImpl.divideCards(
-        in: boardType,
-        with: gameManager.headCount))
+      cards: gameManager.divideCardsToPlayer(in: boardType))
     self.player = .init(cardDeck: playerOwnTheDeck)
   }
 }
 
-extension PlayerCardBoardViewModel {
+extension PlayerCardBoardViewModel: PlayerCardBoardViewModelProtocol {
   func showPlayerCard(_ idx: Int) -> LuckyCard {
     return player.showMyCard(idx)
   }
