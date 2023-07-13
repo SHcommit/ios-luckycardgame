d3b67704042879099a214c43ad10b2cddef9874d
shcommit <happysh_s2@naver.com>
2023-07-14 02:10:46 +0900

    [Test] 플레이어가 선택한 카드가 같은지 다른지 체크하는 함수 테스트 추가

--- LuckyCardGame/LuckyCardGameTests/LuckyGame/SystemUnderTests/Model/SelectedLuckyCardDeckImplTests.swift
+++ LuckyCardGame/LuckyCardGameTests/LuckyGame/SystemUnderTests/Model/SelectedLuckyCardDeckImplTests.swift
@@ -5,4 +5,52 @@
 //  Created by 양승현 on 2023/07/14.
 //
 
-import Foundation
+import XCTest
+@testable import LuckyCardGame
+
+class SelectedLuckyCardDeckImplTests: XCTestCase {
+  let stub = StubLuckyGame(headCount: .five)
+  var sut: SelectedLuckyCardDeckImpl!
+  
+  // MARK: - Lifecycle
+  override func setUp() {
+    super.setUp()
+    stub.setupLuckyCardDeckImls()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+  }
+  
+  // MARK: - Tests
+  func testSelectedLuckyCardDeckImpl_WhenExecutingTheIsCardDeckItemsAllSameWithDifferenceCardsInTheDeck_ShouldReturnEqual() {
+    // Arrange
+    sut = SelectedLuckyCardDeckImpl(
+      cards: (0..<3).map { stub.cardDeck!.cards[$0] })
+    
+    // Act
+    let checkedTheCardsAllSameNumber = sut.isCardDeckItemsAllSame()
+    
+    // Assert
+    XCTAssertEqual(
+      checkedTheCardsAllSameNumber,
+      false,
+      "The isCardDeckItemsAllSame() should have returned Equal for check difference cards in the deck false, but it has returned Not Equal.")
+  }
+  
+  func testSelectedLuckyCardDeckImpl_WhenExecutingTheIsCardDeckItemsAllSameWithSameCardsInTheDeck_ShouldReturnEqual() {
+    // Arrange
+    sut = SelectedLuckyCardDeckImpl(
+      cards: (0..<3).map { _ in return stub.cardDeck!.cards[10] })
+
+    // Act
+    let checkedTheCardsAllSameNumber = sut.isCardDeckItemsAllSame()
+
+    // Assert
+    XCTAssertEqual(
+      checkedTheCardsAllSameNumber,
+      true,
+      "The isCardDeckItemsAllSame() should have returned Equal for check difference cards in the deck false, but it has returned Not Equal.")
+  }
+}
